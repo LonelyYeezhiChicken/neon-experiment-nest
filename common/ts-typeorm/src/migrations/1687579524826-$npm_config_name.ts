786a76ab77e556effa181c029c70f56729e9f2a8
chicken <hibary.taiwan@gmail.com>
2023-06-24 12:06:00 +0800

    feat: 新增 migration

---
+++ common/ts-typeorm/src/migrations/1687579524826-$npm_config_name.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class  $npmConfigName1687579524826 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
