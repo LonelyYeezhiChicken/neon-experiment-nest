291185b352ddd637c1aa3392a525db6ec7d9658d
chicken <hibary.taiwan@gmail.com>
2023-06-24 12:00:38 +0800

    feat: 定義 Entity 內容

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -1,16 +1,44 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
 
-@Entity()
-export class User {
-    @PrimaryGeneratedColumn()
-    id: number
-
-    @Column()
-    firstName: string
-
-    @Column()
-    lastName: string
-
-    @Column()
-    isActive: boolean
+@Entity('book_store', { schema: 'public' })
+export class BookStoreEntity {
+    @PrimaryGeneratedColumn({
+        name: 'id',
+        type: 'bigint',
+    })
+    id: string;
+    @CreateDateColumn({
+        name: 'created_at',
+        type: 'time without time zone',
+    })
+    createdAt: Date;
+    @UpdateDateColumn({
+        name: 'updated_at',
+        type: 'time without time zone',
+    })
+    updatedAt: Date;
+    @Column({
+        name: 'author',
+        type: 'varchar',
+        length: '100',
+    })
+    author: string;
+    @Column({
+        name: 'name',
+        type: 'varchar',
+        length: '100',
+    })
+    name: string;
+    @Column({
+        name: 'publication',
+        type: 'varchar',
+        length: '100',
+    })
+    publication: string;
 }
